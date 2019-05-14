26fbb9ce70063d62a745f44878ed717f410c1120
adefunmi94 <oyeyipoadefunmi90@gmail.com>
2019-05-14 16:53:36 +0100

    i just added an opearator

---
+++ Comparison.java
@@ -0,0 +1,9 @@
+public class Comparison {
+ 
+ public static void main(String[] args) {
+	Scanner.input = new scanner(System.in);
+	System.out.print("Enter first integer:");
+	int number1 = input.nextint();	
+
+	System.out.print("Enter second integer:");
+	int number1 = input.nextint();
