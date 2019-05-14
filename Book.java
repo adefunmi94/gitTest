b1fc8fc51eea3494141a42e95ab9ec7b3554da36
adefunmi94 <oyeyipoadefunmi90@gmail.com>
2019-05-14 16:10:29 +0100

    new set and get

--- Book.java
+++ Book.java
@@ -3,6 +3,18 @@
  	private int pages;
 	private String author;
 	private String title:
-
+	
+	public void setauthor(String author) {
+	this.author = author;
+		}
+	public String getauthor();
+	return author;
+	}
+	public void setisHardcover() {
+	this.isHardcover = true;
+	}
+	public boolean getisHardcover() {
+	return isHardcover;
+	}
  } 
 		
